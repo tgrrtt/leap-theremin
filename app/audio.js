200cb903de02914d07d1a46fca2e288bda810f15
Tyler Garrett <tyler@tgrrtt.com>
2015-01-02 23:56:39 -0800

    Implemented pinking & moog filter
    not sure how they sound because pitch cant change yet.

--- app/audio.js
+++ app/audio.js
@@ -4,10 +4,67 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 // create Oscillator and gain node
 var oscillator = audioCtx.createOscillator();
 var gainNode = audioCtx.createGain();
+var bufferSize = 4096;
+
+var pinkingFilter = (function() {
+    var b0, b1, b2, b3, b4, b5, b6;
+    b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+    var node = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            b0 = 0.99886 * b0 + input[i] * 0.0555179;
+            b1 = 0.99332 * b1 + input[i] * 0.0750759;
+            b2 = 0.96900 * b2 + input[i] * 0.1538520;
+            b3 = 0.86650 * b3 + input[i] * 0.3104856;
+            b4 = 0.55000 * b4 + input[i] * 0.5329522;
+            b5 = -0.7616 * b5 - input[i] * 0.0168980;
+            output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + input[i] * 0.5362;
+            output[i] *= 0.11; // (roughly) compensate for gain
+            b6 = input[i] * 0.115926;
+        }
+    }
+    return node;
+})();
+
+var moogFilter = (function() {
+    var node = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+    var in1, in2, in3, in4, out1, out2, out3, out4;
+    in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0;
+    node.cutoff = 0.065; // between 0.0 and 1.0
+    node.resonance = 3.99; // between 0.0 and 4.0
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        var f = node.cutoff * 1.16;
+        var fb = node.resonance * (1.0 - 0.15 * f * f);
+        for (var i = 0; i < bufferSize; i++) {
+            input[i] -= out4 * fb;
+            input[i] *= 0.35013 * (f*f)*(f*f);
+            out1 = input[i] + 0.3 * in1 + (1 - f) * out1; // Pole 1
+            in1 = input[i];
+            out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
+            in2 = out1;
+            out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
+            in3 = out2;
+            out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
+            in4 = out3;
+            output[i] = out4;
+        }
+    }
+    return node;
+})();
 
 // connect oscillator to gain node to speakers
 
-oscillator.connect(gainNode);
+// for rn, watch moog value and set the volume accordingly.
+// the moog filter drastically lowers sound volume so we have to use something like 5 for default,
+// whereas a 5 would normally break ur speakers.
+var moog = true;
+
+oscillator.connect(moogFilter);
+moogFilter.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 
 // create initial theremin frequency and volumn values
@@ -18,10 +75,9 @@ var maxVol = 0.02;
 var initialFreq = 3000;
 
 // .00001 increments work
-var initialVol = 0.0000;
+var initialVol = 0;
 
 // set options for the oscillator
-
 oscillator.type = 'sine';
 oscillator.frequency.value = initialFreq; // value in hertz
 // oscillator.detune.value = 100; // value in cents
@@ -43,12 +99,12 @@ Leap.loop(controllerOptions, function(frame) {
     var left = frame.hands[0].type === "left" ? frame.hands[0] : frame.hands[1];
     // palmPosition is an array with [x,y,z] coordinates.
     var volumeCoordinate = left.palmPosition[1];
-    
+
     // volume should go from y = 100 to y = 300?
     volumeCoordinate = Math.max(volumeCoordinate, 100);
-    volumeCoordinate = Math.min(volumeCoordinate, 300);
+    volumeCoordinate = Math.min(volumeCoordinate, 400);
 
-    var volume = (volumeCoordinate - 100)/10000;
+    var volume = moog? (volumeCoordinate - 100)/ 10: (volumeCoordinate - 100)/10000;
 
     gainNode.gain.value = volume || 0;
   }
